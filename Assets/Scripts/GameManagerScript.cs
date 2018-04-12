ca8b9461d9b0bdac5cf1db39b690f202ff2a9026
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2018-04-12 10:45:17 +1000

    rfsd
    sdfsdf

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour 
+{
+	public Text[] numbersText = new Text [1];
+	public int[] numbers = new int[1];
+
+	public float time;
+	public float roundLength;
+
+	public Text timeText;
+
+	//buttons
+	public Button [] buttons;
+
+	//answer
+
+	public float Answer;
+
+	void Start () 
+	{
+		time = 0.1f;
+	}
+	
+	void Update () 
+	{
+		time -= Time.deltaTime;
+		timeText.text = "Time : " + time.ToString ();
+
+		if (time <= 0)
+		{
+			time = roundLength;
+			randomNums();
+			Answer = numbers [0] + numbers [1];
+			displayResults ();
+		}
+	}
+
+	void randomNums()
+	{
+		for (int i = 0; i < numbers.Length; i++) 
+		{
+			numbers [i] = Random.Range (0, 10);
+			numbersText [i].text = numbers [i].ToString ();
+		}
+
+		if (p.Level < 40f) {
+			Timer = roundLength;
+		} 
+
+		else 
+		{
+			Timer = 10f;
+		}
+
+		result = numbers [0] + numbers [1];
+		Debug.Log (result);
+	}
+
+	void displayResults()
+	{
+		for (int i = 0; i < buttons.Length; i++) 
+		{
+			buttons [i].gameObject.GetComponent<Text> ().text = Answer.ToString();
+		}
+	}
+}
